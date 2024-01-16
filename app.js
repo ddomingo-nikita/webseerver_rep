d9e240fb89951774b80af451c0c0f6989c29a7e0
NikitaEfimov0 <nikitaef2002@gmail.com>
2024-01-16 14:12:50 +0300

    added home page and redirection

--- app.js
+++ app.js
@@ -1,28 +1,57 @@
 import express from "express"
 import {database} from "./database.js";
 
+import cookieParser from "cookie-parser"
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express()
+
+
 
 const port = 3001
 
+
+app.use(cookieParser())
 app.use(express.static('public'));
 
 app.use(express.urlencoded({extended: true}))
-app.get("/", (req, res)=>{
+app.get("/home", (req, res)=>{
 
-    res.sendFile("index.html")
+    res.sendFile(__dirname+"/public/home.html")
+})
+
+app.get("/", (req, res)=>{
+    res.redirect(301, "/home")
+})
+
+app.get("/login", (req, res)=>{
+    res.sendFile(__dirname+"/public/login.html")
 })
 
 app.post("/api/login", (req, res) => {
     const name = req.body.username;
     const password = req.body.password;
     if(database.users.find((user)=>user.name===name && user.password===password)){
-        res.send("you are logged in")
+        const newToken = `${name}_${Date.now()}`
+        res.cookie('token', newToken, { maxAge: 12000, httpOnly: false});
+        res.send(newToken)
     }
     else{
-        res.send("Wrong info")
+        res.sendStatus(401)
     }
 
+})
+
+app.get("/dashboard", (req, res)=>{
+    const isLogged = req.cookies.token
+    console.log(isLogged)
+    if(isLogged){
+        res.send("welcome to the dashboard")
+    }
+    else{
+        res.redirect(301, "/")
+    }
 })
 
 
