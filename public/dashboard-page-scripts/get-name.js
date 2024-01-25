7a7cfa21d7cd522f9c10bd3d5096cce7e5a190bd
NikitaEfimov0 <nikitaef2002@gmail.com>
2024-01-25 16:24:33 +0300

    added images

--- public/dashboard-page-scripts/get-name.js
+++
@@ -1,6 +0,0 @@
-const getName = () => {
-    document.getElementById("name").textContent=document.cookie.split("=")[1].split("_")[0]
-}
-
-
- window.onload = () => getName()
--- public/login-page-scripts/password-toggle.js
+++
@@ -1,7 +0,0 @@
-const togglePasswordField = () => {
-    const input = document.getElementById("password-input")
-    const button = document.getElementById("show-button")
-    input.type = input.type==="password" ? "text" : "password"
-    button.textContent = button.textContent === "Show" ? "Hide" : "Show"
-}
-
